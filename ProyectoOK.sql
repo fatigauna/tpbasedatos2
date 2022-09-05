35927341fe71f7630de3de85feef53c032e9aec1
JuanRoma <juangaroma18@gmail.com>
2022-09-05 19:20:18 -0300

    otro cambio

--- ProyectoOK.sql
+++ ProyectoOK.sql
@@ -12,31 +12,3 @@ CALL spCrear_Proyecto2 ( 2 , 'Tech Giants' , 'b');
 
 
 
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `cargar_horas_semana`(in emp_id int, in pro_id int, in rol_id int, in cli_id int, in horas int)
-BEGIN
-
-
-    set @cont = 0;
-    set @target = 6;
-
-    while @cont <= @target do
-
-            insert into hora(emp_legajo, pro_id_proyecto, rol_id_rol, cli_id_cliente, hor_fecha, hor_horas_dia)
-            values (emp_id, pro_id, rol_id, cli_id, DATE_ADD(SYSDATE(), INTERVAL -@cont DAY), horas);
-
-            set @cont = @cont + 1;
-
-    end while;
-END; $$
-
- 
- USE tpbdd2;
-CALL cargar_horas_semana (4,4,9,1,10);
-CALL cargar_horas_semana (1,4,1,1,10);
-CALL cargar_horas_semana (2,4,2,1,10);
-CALL cargar_horas_semana (3,4,3,1,10);
-CALL cargar_horas_semana (5,4,4,1,10);
-CALL cargar_horas_semana (6,4,5,1,10);
-CALL cargar_horas_semana (7,4,9,1,100);
-CALL cargar_horas_semana (8,4,6,1,1);
